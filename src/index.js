02793b0ba161f4a653ffe68e846209036d31289d
anandpothraj <anandpothraj11052001@gmail.com>
2023-05-07 15:24:01 +0530

    Integrated Bootstrap, customized dark theme

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './index.css';
 import App from './App';
 import React from 'react';
+import './assets/customCss/theme.css';
 import ReactDOM from 'react-dom/client';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
