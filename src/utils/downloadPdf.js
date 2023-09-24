5a06938a53715f7de4407a105a2fe4cb499f1b1c
anandpothraj <anandpothraj11052001@gmail.com>
2023-09-24 16:58:43 +0530

    enhanced downloadPdf function

--- src/utils/downloadPdf.js
+++ src/utils/downloadPdf.js
@@ -5,38 +5,44 @@ import server from '../config/server.json';
 export const downloadPdf = async (vaccineName, vaccineDose, id) => {
     const production = server.url.production;
     const DOWNLOAD_CERTIFICATE = server.api.patients.DOWNLOAD_CERTIFICATE;
+    
     try {
-        const res = await axios.get(`${production}${DOWNLOAD_CERTIFICATE}/${id}`,{
+        const res = await axios.get(`${production}${DOWNLOAD_CERTIFICATE}/${id}`, {
             responseType: 'arraybuffer',
-        }); 
+        });
+
         if (res.status === 200) {
             const pdfData = res.data;
 
             // Create a Blob from the PDF data
             const blob = new Blob([pdfData], { type: 'application/pdf' });
-    
+
             // Create a URL for the Blob
             const url = URL.createObjectURL(blob);
-    
+
             // Create an anchor element to trigger the download
             const a = document.createElement('a');
             a.href = url;
-    
+
             // Set the download attribute with the desired filename
             a.download = `${vaccineName}_0${vaccineDose}.pdf`;
-    
+
             // Simulate a click on the anchor element to trigger the download
             a.click();
-    
+
             // Clean up by revoking the URL
             URL.revokeObjectURL(url);
+        } else {
+            // Handle specific error status codes
+            if (res.status === 404) {
+                notify("error", "PDF not found.");
+            } else {
+                notify("error", "Error while generating PDF.");
+            }
         }
-        else{
-            notify("error", "Error while generating PDF!");
-        }
-    } 
-    catch (err) {
+    } catch (err) {
+        // Handle network errors or unexpected errors
         console.log(err);
-        notify("error", err.response.data.message);
+        notify("error", "An error occurred while downloading the PDF.");
     }
 }
