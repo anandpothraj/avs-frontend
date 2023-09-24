cbe8cd74287a40da9c7b99c07f8186c7c60a8826
anandpothraj <anandpothraj11052001@gmail.com>
2023-09-24 16:45:28 +0530

    notifying download pdf error

--- src/utils/downloadPdf.js
+++ src/utils/downloadPdf.js
@@ -8,31 +8,34 @@ export const downloadPdf = async (vaccineName, vaccineDose, id) => {
     try {
         const res = await axios.get(`${production}${DOWNLOAD_CERTIFICATE}/${id}`,{
             responseType: 'arraybuffer',
-        });
-        
-    if (res.status === 200) {
-        const pdfData = res.data;
-  
-        // Create a Blob from the PDF data
-        const blob = new Blob([pdfData], { type: 'application/pdf' });
-  
-        // Create a URL for the Blob
-        const url = URL.createObjectURL(blob);
-  
-        // Create an anchor element to trigger the download
-        const a = document.createElement('a');
-        a.href = url;
-  
-        // Set the download attribute with the desired filename
-        a.download = `${vaccineName}_0${vaccineDose}.pdf`;
-  
-        // Simulate a click on the anchor element to trigger the download
-        a.click();
-  
-        // Clean up by revoking the URL
-        URL.revokeObjectURL(url);
-      }
-    } catch (err) {
+        }); 
+        if (res.status === 200) {
+            const pdfData = res.data;
+
+            // Create a Blob from the PDF data
+            const blob = new Blob([pdfData], { type: 'application/pdf' });
+    
+            // Create a URL for the Blob
+            const url = URL.createObjectURL(blob);
+    
+            // Create an anchor element to trigger the download
+            const a = document.createElement('a');
+            a.href = url;
+    
+            // Set the download attribute with the desired filename
+            a.download = `${vaccineName}_0${vaccineDose}.pdf`;
+    
+            // Simulate a click on the anchor element to trigger the download
+            a.click();
+    
+            // Clean up by revoking the URL
+            URL.revokeObjectURL(url);
+        }
+        else{
+            notify("error", "Error while generating PDF!");
+        }
+    } 
+    catch (err) {
         console.log(err);
         notify("error", err.response.data.message);
     }
