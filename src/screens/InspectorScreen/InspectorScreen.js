2657c1157def49ad6a7b2c713f5d1468a3ebb832
anandpothraj <anandpothraj11052001@gmail.com>
2023-10-05 14:38:02 +0530

    created inspector screen

--- src/screens/InspectorScreen/InspectorScreen.js
+++ src/screens/InspectorScreen/InspectorScreen.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
+import { CgProfile } from 'react-icons/cg';
+import { TbVaccine } from 'react-icons/tb';
+import { GiOverdose } from 'react-icons/gi';
 import { notify } from '../../utils/notify';
+import { RiCake2Line } from 'react-icons/ri';
 import server from '../../config/server.json';
+import { FaUserShield } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import MainScreen from '../../layout/MainScreen';
 import React, { useEffect, useState } from 'react';
 import { redirectUser } from '../../utils/redirectUser';
+import { HiOutlineIdentification } from 'react-icons/hi';
 import { collapseNavbar } from '../../utils/collapseNavbar';
-import { Alert, Badge, Button, Container, Form, Spinner } from 'react-bootstrap';
-import { FaUser } from 'react-icons/fa';
-import { MdMale } from 'react-icons/md';
-import { TbVaccine } from 'react-icons/tb';
-import { GiOverdose } from 'react-icons/gi';
+import { formatDateString } from '../../utils/formatDateString';
+import { BsCalendar2Date, BsGenderAmbiguous } from 'react-icons/bs';
+import { Badge, Button, Container, Form, Spinner } from 'react-bootstrap';
 
 const InspectorScreen = () => {
 
@@ -21,12 +25,23 @@ const InspectorScreen = () => {
     vaccineName : initialBlankValue
   }]);
   const [ aadhaar, setAadhaar ] = useState("");
-  const [ loading, setLoading ] = useState(true);
+  const [ loading, setLoading ] = useState(false);
+  const [ patientInfo, setPatientInfo ] = useState(null);
   const [ vaccinations, setVaccinations ] = useState([]);
   const FETCH_VACCINES = server.api.doctors.FETCH_VACCINES;
+  const [ showPatientInfo, setShowPatientInfo ] = useState(false);
   const [ vaccineDose, setVaccineDose ] = useState([initialBlankValue]);
   const [ selectedDose, setSelectedDose ] = useState(initialBlankValue);
   const [ selectedVaccine, setSelectedVaccine ] = useState(initialBlankValue);
+  const FETCH_VACCINATIONS_INFO = server.api.inspectors.FETCH_VACCINATIONS_INFO;
+
+  const generateQuery = (aadhaar, selectedVaccine, selectedDose) => {
+    let query = "?";
+    query += aadhaar ? `aadhaar=${aadhaar}` : '';
+    query += selectedVaccine !== "--select--" ? `&vaccineName=${selectedVaccine}` : '';
+    query += selectedDose !== "--select--" ? `&doseNo=${selectedDose}` : '';
+    return query;
+  }
 
   const updateNoOfDoseArray = (maxDose) => {
     const noOfDoseArray = Array.from({ length: maxDose }, (_, index) => index + 1);
@@ -65,21 +80,51 @@ const InspectorScreen = () => {
       console.log(err);
       notify("error",err.response.data.message);
     })
-    // setLoading(false);
+    setLoading(false);
   }
 
   const resetFields = () => {
     setAadhaar("");
+    setVaccinations([]);
+    setShowPatientInfo(false);
     setSelectedDose(initialBlankValue);
     setSelectedVaccine(initialBlankValue);
   }
 
+  const fetchVaccinationsInfo = async (query) => {
+    setLoading(true);
+    await axios
+    .get(`${production}${FETCH_VACCINATIONS_INFO}/${query}`)
+    .then(res => {
+      if(res.status === 200){
+        setShowPatientInfo(true);
+        setPatientInfo(res.data.patientInfo);
+        if(res.data.vaccinations.length > 0 ){
+          setVaccinations(res.data.vaccinations);
+        }
+        else{
+          setVaccinations([]);
+          notify("error", res.data.message);
+        }
+        setAadhaar("");
+        setSelectedVaccine(initialBlankValue);
+        setSelectedDose(initialBlankValue);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      notify("error",err.response.data.message);
+    })
+    setLoading(false);
+  }
+
   const searchVaccinations = () => {
-    if(aadhaar && selectedVaccine !== initialBlankValue && selectedDose !== initialBlankValue){
-      console.log(aadhaar, selectedVaccine, selectedDose);
+    if(aadhaar && aadhaar.length === 12){
+      let query = generateQuery(aadhaar, selectedVaccine, selectedDose);
+      fetchVaccinationsInfo(query);
     }
     else{
-      notify("error", "Please fill all the fields!");
+      notify("error", "Invalid Input, Aadhaar should only consist 12 digits!");
     }
   }
 
@@ -124,36 +169,46 @@ const InspectorScreen = () => {
                 )))
               }
             </select>
-            <Button className="btn btn-success my-1 mx-auto w-50" onClick={searchVaccinations} disabled={!aadhaar}>{loading && <Spinner size="sm" as="span" className="mx-2"/>}Search</Button>
+            <Button className="btn btn-success my-1 mx-auto w-50" onClick={searchVaccinations} disabled={!aadhaar || loading}>{loading && <Spinner size="sm" as="span" className="mx-2"/>}Search</Button>
         </Form>
         <hr />
-        {/* {loading && <div className="d-flex align-items-center"><Spinner as="span"/><span className='mx-3'>Fetching Vaccination Status...</span></div>} */}
-        <Alert variant='light' className='overflow-auto '>
-          <span>Patient Information</span>
-          <span className='mx-2 text-info'><FaUser className='mx-2'/>Anand Narsappa Pothraj</span>
-          <span className='mx-2 text-danger'>22 Age</span>
-          <span className='mx-2 text-warning'>
-              {/* {patientGender === "Male" ? <MdMale className='mx-2'/> : <MdFemale className='mx-2'/>}{patientGender} */}
-              <MdMale className='mx-2'/> Male
-          </span>
-        </Alert>
-        <Alert variant='light' className='d-flex flex-row align-items-center overflow-auto justify-content-start justify-content-lg-around'>
-        <span>
-            <Badge bg="primary" >Vaccination Info</Badge>
-          </span>
-          <span className='mx-2 text-success' >
-              <TbVaccine className='mx-2' /> Covaxin
-          </span>
-          <span className='mx-2 text-primary'>
-              <GiOverdose className='mx-2' /> 1
-          </span>
-          <span>
-            <Badge bg="danger" >Partially Vaccinate</Badge>
-          </span>
-        </Alert>
+        {loading ?
+        <>
+          <div className="d-flex align-items-center"><Spinner as="span"/><span className='mx-3'>Fetching Vaccination Status...</span></div>
+          <hr />
+        </>
+        : 
+        <>
+          {showPatientInfo && patientInfo && 
+            <Container className='overflow-auto bg-light p-2'>
+              <Badge bg='success' className='my-2'>Patient Information</Badge>
+              <p className='my-1'><CgProfile color='red' className='mx-2'/>Patient Name : {patientInfo.name}</p>
+              <p className='my-1'><HiOutlineIdentification color='green' className='mx-2'/>Patient Aadhaar : {patientInfo.aadhaar}</p>
+              <p className='my-1'><BsGenderAmbiguous color='cyan' className='mx-2'/>Patient Gender : {patientInfo.gender}</p>
+              <p className='my-1'><RiCake2Line color='orange' className='mx-2'/>Patient Age : {patientInfo.age}</p>
+            </Container>
+          }
+          {vaccinations && vaccinations.length > 0 && <Badge bg="info" className='p-2 my-2'>Vaccination Info</Badge>}
+          {
+            vaccinations && vaccinations.length > 0 ?
+            vaccinations.map((vaccination, index) => {
+              return (
+                <Container className='overflow-auto bg-light p-2 my-2' key={index}>
+                <p className='my-1'><FaUserShield color='aqua'  className='mx-2'/>Vaccination Status : {
+                  vaccination.fullyVaccinated ? <Badge bg='success' className='mx-2'>Fully Vaccinated</Badge> : <Badge bg='danger' className='mx-2'>Partially Vaccinated</Badge>
+                }</p>
+                <p className='my-1'><TbVaccine color='lime' className='mx-2'/>Vaccine Name : {vaccination.vaccineName}</p>
+                <p className='my-1'><GiOverdose color='fuchsia' className='mx-2'/>Vaccine Dose No : {vaccination.doseNo}</p>
+                <p className='my-1'><BsCalendar2Date color='grey' className="mx-2"/>Vaccinated On : {formatDateString(vaccination.updatedAt)}</p>
+                </Container>
+              )
+            }) : null
+          }
+        </>  
+        }
       </Container>
     </MainScreen>
-  )
-}
+  );
+};
 
-export default InspectorScreen
+export default InspectorScreen;
